6fd51cbcb041420ef513ee549332a82efe72ae9f
auriafoxgirl <53555592+auriafoxgirl@users.noreply.github.com>
2026-06-21 20:06:28 +0200

    improve fish colors

--- code/fish.lua
+++ code/fish.lua
@@ -27,6 +27,13 @@ for i, v in ipairs(syllablesData) do
    syllablesLookup[v.text] = v
 end
 
+local letterColors = {
+   b = 0.52,
+   v = 0.8,
+   l = 0.25,
+   sh = 0.1
+}
+
 local namePrefixes = {
    {value = "", weight = 35},
    {value = "golden ", weight = 1},
@@ -34,6 +41,13 @@ local namePrefixes = {
 
 local default0Mt = {
    __index = function() return 0 end
+}
+
+local extraFish = {
+   [3] = {
+      {value = "", weight = 200},
+      {value = "labubu", weight = 1}
+   }
 }
 
 ---@param seed number?
@@ -46,8 +60,14 @@ function mod.makeFishName(seed)
    local hadVowel = false
    name = name..utils.weightedRandom(namePrefixes, utils.seededRand(seed + 1))
    local syllablesCount = utils.seededRandInt(seed + 2, utils.seededRandInt(seed + 3, 4) + 2) + 1
+   if extraFish[syllablesCount] then
+      local customName = utils.weightedRandom(extraFish[syllablesCount], utils.seededRand(seed + 5))
+      if customName ~= "" then
+         return name..customName.." fish"
+      end
+   end
    for i = 1, syllablesCount do
-      local rand = utils.seededRandInt(seed + 3 + i, maxSyllables)
+      local rand = utils.seededRandInt(seed + 10 + i, maxSyllables)
       for k = 1, maxSyllables do
          local n = (k + rand) % maxSyllables + 1
          local v = syllablesData[n]
@@ -62,17 +82,57 @@ function mod.makeFishName(seed)
    return name.." fish"
 end
 
+---@param seed number
+---@param hue number
+---@param hueStrength number
+---@param name string
+---@return Vector3[]
+local function generateColors(seed, hue, hueStrength, name)
+   local colors = {}
+   local customHue = 0
+   local hueSum = 0.0001
+   for i, v in pairs(letterColors) do
+      local _, c = name:gsub(i, i)
+      customHue = customHue + v * c
+      hueSum = hueSum + c
+   end
+   customHue = math.lerp(
+      customHue / hueSum,
+      utils.seededRand(seed),
+      utils.seededRand(seed + 3)
+   )
+   hueStrength = 0
+   hue = math.lerp(
+      customHue,
+      hue,
+      hueStrength
+   )
+   local k = (utils.seededRand(seed + 1) - 0.5) * 0.2 * (1 - hueStrength)
+   if utils.seededRand(seed + 2) > 0.7 then
+      k = k + 0.333
+   end
+   k = 0
+   for i = 1, 3 do
+      colors[i] = vectors.hsvToRGB(
+         hue + (i - 1) * k,
+         utils.seededRand(seed + 6 + i * 20) ^ 2 * 0.7,
+         utils.seededRand(seed + 7 + i * 20) * 0.7 + 0.3
+      )
+   end
+   return colors
+end
+
 ---@param rawFishName string
 ---@return ModelPart[]
 function mod.generateFishModel(rawFishName)
    local fishName = rawFishName
-   local hueOffset = 0
-   local hueScale = 1
+   local hue = 0
+   local hueStrength = 0
 
    if fishName:sub(1, 7) == "golden " then
       fishName = fishName:sub(8, -1)
-      hueOffset = 0.1
-      hueScale = 0.04
+      hue = 0.12
+      hueStrength = 0.95
    end
 
    local model = models:newPart(""):remove()
@@ -83,20 +143,13 @@ function mod.generateFishModel(rawFishName)
    local layerCount = utils.seededRandInt(seed + 1, 3)
    local fishStyle = utils.seededRandInt(seed + 3, 3) - 1
 
-   local colors = {}
-   for i = 1, layerCount do
-      colors[i] = vectors.hsvToRGB(
-         utils.seededRand(seed + 5 + i * 20) * hueScale + hueOffset,
-         utils.seededRand(seed + 6 + i * 20) ^ 2 * 0.7,
-         utils.seededRand(seed + 7 + i * 20) * 0.7 + 0.3
-      )
-   end
+   local colors = generateColors(-seed, hue, hueStrength, fishName)
 
    for i = 0, layerCount do
       local layerY = i - 1
       local k = i
       if i == 0 then
-         layerY = 2 + utils.seededRandInt(seed + 4, 2)
+         layerY = 2 + utils.seededRandInt(seed + 5, 2)
          k = 0.5
       elseif layerCount == 2 and i == 2 then
          layerY = utils.seededRandInt(seed + 2, 2)
@@ -160,11 +213,11 @@ local fishName = mod.makeFishName()
 models:newPart("", "Hud")
    :setLight(15, 15)
    :setScale(4, 4)
-   :setPos(vec(-9, -9, 0) * 4)
+   :setPos(vec(-10, -10, 0) * 4)
    :addChild(mod.generateFishModel(fishName)[4])--:setPos(-16, -16))
    :newText("a")
    :setText(fishName)
-   :setPos(7, -9)
+   :setPos(7, -10)
    -- :setPos(-0.5, -16.5, 0)
    :setScale(1 / 2)
    :setOutline(true)
